0a4a7b738297f632e5c0b93628e2a24b1f147806
Bruno Juan <bruno.kirovs@gmail.com>
2025-06-26 23:25:16 -0300

    subindo repositorio

---
+++ src/hello/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string = "Bruno"
+	var versao float32 = 1.1
+	fmt.Println("Ola, sr.", nome)
+	fmt.Println("Está programa está na versão", versao)
+}
