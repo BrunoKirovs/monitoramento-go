c61755267e97021cb9354d5ee5aca768bda35ddf
Bruno Juan <bruno.kirovs@gmail.com>
2025-06-30 21:48:31 -0300

    controle de fluxos

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -3,8 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	var nome string = "Bruno"
-	var versao float32 = 1.1
+	nome := "Bruno"
+	versao := 1.1
 	fmt.Println("Ola, sr.", nome)
 	fmt.Println("Está programa está na versão", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do programa")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O comando escolhido foi", comando)
+
+	//if comando == 1 {
+	//	fmt.Println("Monitorando...")
+	//} else if comando == 2 {
+	//	fmt.Println("Exibindo Logs...")
+	//} else if comando == 0 {
+	//	fmt.Println("Saindo do programa")
+	//} else {
+	//	fmt.Println("Nao conheço esse comando")
+	//}
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa")
+	default:
+		fmt.Println("Não conheço este comando")
+	}
+
 }
