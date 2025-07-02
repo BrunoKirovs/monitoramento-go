66663890eb8cc50b90f6344350e027aac22faf5d
Bruno Juan <bruno.kirovs@gmail.com>
2025-07-01 21:23:59 -0300

    trabalhando funções

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,30 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	nome := "Bruno"
-	versao := 1.1
-	fmt.Println("Ola, sr.", nome)
-	fmt.Println("Está programa está na versão", versao)
 
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do programa")
+	exibeIntroducao()
+	exebirMenu()
 
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("O comando escolhido foi", comando)
-
-	//if comando == 1 {
-	//	fmt.Println("Monitorando...")
-	//} else if comando == 2 {
-	//	fmt.Println("Exibindo Logs...")
-	//} else if comando == 0 {
-	//	fmt.Println("Saindo do programa")
-	//} else {
-	//	fmt.Println("Nao conheço esse comando")
-	//}
+	comando := leComando()
 
 	switch comando {
 	case 1:
@@ -33,8 +19,31 @@ func main() {
 		fmt.Println("Exibindo Logs...")
 	case 0:
 		fmt.Println("Saindo do programa")
+		os.Exit(0)
 	default:
 		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
 	}
 
+}
+
+func exibeIntroducao() {
+	nome := "Bruno"
+	versao := 1.1
+	fmt.Println("Ola, sr.", nome)
+	fmt.Println("Está programa está na versão", versao)
+}
+
+func exebirMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
 }
